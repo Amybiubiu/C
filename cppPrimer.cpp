a2155cdcfe010c53ac87b37e7abcf2abdc4e35f8
Amybiubiu <1246457100@qq.com>
2020-06-22 21:33:41 +0800

    & * const

---
+++ cppPrimer.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+//pointer
+void pointer(void){
+    int i = 42;
+    int *p = &i;
+    *p = 2;
+    cout<<i<<endl;  //2
+}
+//reference
+void ref(){
+    int i = 54;
+    int &r = i;
+    int i2 = 24;
+    r = i2;
+    // i = 23; r->23
+    i2 = 33;
+    cout<<r<<endl;  //24
+}
+//const reference
+void const_ref(){
+    int i = 54;
+    int &r = i;
+    const int &r1 = i;
+    i = 23;
+    cout<<r<<endl;
+    cout<<r1<<endl;    //23 r1 is const reference to r1;
+    //r1 = 0; error, can't change be used to change i
+    //这种设计真的绕的人晕
+}
+int main(){
+    pointer();  //为什么会引用不了？？函数顺序问题++
+    int i = 54;
+    int &r = i;
+    int i2 = 24;
+    r = i2;
+    // i = 23;
+    i2 = 33;
+    cout<<r<<endl;  //23
+    return 0;
+}
