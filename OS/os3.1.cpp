499e5ca96e18cf16459c64b8b0cbda1ca8a45238
Amybiubiu <1246457100@qq.com>
2020-12-17 16:45:11 +0800

    os exercise 3 、4

--- OS/os3.1.cpp
+++ OS/os3.1.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 vector<pair<int, int> > item_table;
+vector<pair<int, int> > allocate_table;
 
 void display(){
     cout<<"————起始地址——空闲区大小————"<<endl;
@@ -10,7 +11,13 @@ void display(){
         cout<<"      "<<item_table[i].first<<"     "<<item_table[i].second<<endl;
     }
 }
-
+void display_allocate(){
+    cout<<"——进程序号——起始地址——空闲区大小————"<<endl;
+    for(int i = 0; i < allocate_table.size(); i++){
+        cout<<"    "<< i << " ";
+        cout<<"      "<<allocate_table[i].first<<"     "<<allocate_table[i].second<<endl;
+    }
+}
 void init(){
     item_table.push_back(make_pair(0, 100));
     item_table.push_back(make_pair(100, 50));
@@ -20,39 +27,87 @@ void init(){
 
 void first(vector<int>& prcs){
     int n = prcs.size();
-    vector<int> res(n, 0);
     for(int i = 0; i < n; i++){
         for(auto j = item_table.begin(); j != item_table.end(); j++){
             // 找到fit
             if(j->second == prcs[i]){
-                res[i] = 1;
+                allocate_table.push_back(make_pair(j->first, j->second));
                 item_table.erase(j);
                 break;
             }else if(j->second > prcs[i]){
-                res[i] = 1;
+                allocate_table.push_back(make_pair(j->first, prcs[i]));
                 j->first += prcs[i];
                 j->second -= prcs[i];
                 break;
             }
         } 
     }
+    cout<<"------首次适应算法-------"<<endl;
     display();
-    for(int i = 0; i < n; i++){
-        cout<<res[i]<<" ";
-    }
-    cout<<endl;
+    display_allocate();
+    allocate_table.clear();
 }
 
 void best(vector<int>& prcs){
-
+    int n = prcs.size();
+    for(int i = 0; i < n; i++){
+        auto target = item_table.begin();   // 需要一些排错
+        int min_dis = INT8_MAX;
+        for(auto j = item_table.begin(); j != item_table.end(); j++){
+            // 找到fit
+            if((j->second - prcs[i]) >= 0 && (j->second - prcs[i]) < min_dis)
+                target = j; 
+        }
+        if (target->second == prcs[i])
+        {
+            allocate_table.push_back(make_pair(target->first, target->second));
+            item_table.erase(target);
+        }
+        else if (target->second > prcs[i])
+        {
+            allocate_table.push_back(make_pair(target->first, prcs[i]));
+            target->first += prcs[i];
+            target->second -= prcs[i];
+        }
+    }
+    cout<<"------最佳适应算法-------"<<endl;
+    display();
+    display_allocate();
+    allocate_table.clear();
 }
 void bad(vector<int>& prcs){
-    
+    int n = prcs.size();
+    for(int i = 0; i < n; i++){
+        auto target = item_table.begin();   // 需要一些排错
+        int max_dis = 0;
+        for(auto j = item_table.begin(); j != item_table.end(); j++){
+            // 找到fit
+            if((j->second - prcs[i]) >= 0 && (j->second - prcs[i]) > max_dis)
+                target = j; 
+        }
+        if (target->second == prcs[i])
+        {
+            allocate_table.push_back(make_pair(target->first, target->second));
+            item_table.erase(target);
+        }
+        else if (target->second > prcs[i])
+        {
+            allocate_table.push_back(make_pair(target->first, prcs[i]));
+            target->first += prcs[i];
+            target->second -= prcs[i];
+        }
+    }
+    cout<<"------最差适应算法-------"<<endl;
+    display();
+    display_allocate();
+    allocate_table.clear();
 }
 int main(){
     int arr[5] = {80, 30, 28, 10, 10};
     vector<int> prcs(arr, arr+5);
     init();
-    first(prcs);
+    // first(prcs);
+    // best(prcs);
+    bad(prcs);
     return 0;
 }
---
+++ OS/os4.1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void FIFO(vector<int>& pages){
+    cout<<"-----FIFO------"<<endl;
+    double n = pages.size();
+    int pagefault = 0;
+    double pagefault_rate = 0;
+    vector<int> memory_block(3, 0);
+    for(int i = 0; i < n; i++){
+        auto target = find(memory_block.begin(), memory_block.end(), pages[i]);
+        if(target != memory_block.end()){
+            // hit
+            cout<<"0 ";
+        }else{
+            // pagefault
+            pagefault++;
+            cout<<memory_block[2]<<" ";
+            memory_block.erase(memory_block.begin() + 2);
+            memory_block.insert(memory_block.begin(), pages[i]);
+        }
+    }
+    pagefault_rate = pagefault / n;
+    cout<<"\n";
+    cout<<"中断次数："<<pagefault<<" 缺页率："<<pagefault_rate<<endl;
+}
+
+void LRU(vector<int>& pages){
+    cout<<"-----LRU------"<<endl;
+    double n = pages.size();
+    int pagefault = 0;
+    double pagefault_rate = 0;
+    vector<int> memory_block(3, 0);
+    for(int i = 0; i < n; i++){
+        auto target = find(memory_block.begin(), memory_block.end(), pages[i]);
+        if(target != memory_block.end()){
+            // hit
+            cout<<"0 ";
+            memory_block.erase(target);
+            memory_block.insert(memory_block.begin(), pages[i]);
+        }else{
+            // pagefault
+            pagefault++;
+            cout<<memory_block[2]<<" ";
+            memory_block.erase(memory_block.begin() + 2);
+            memory_block.insert(memory_block.begin(), pages[i]);
+        }
+    }
+    pagefault_rate = pagefault / n;
+    cout<<"\n";
+    cout<<"中断次数："<<pagefault<<" 缺页率："<<pagefault_rate<<endl;
+}
+
+int main(){
+    int arr[12] = {1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5};
+    vector<int> pages(arr, arr+12);
+    FIFO(pages);
+    LRU(pages);
+}
